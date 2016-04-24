fcb99c1cb0dafcd4474b528efdb9e403e89174ed
Vip-Augus <850366301@qq.com>
2016-04-24 11:32:32 +0800

    Update LongRunningService.java

--- ServiceBestPractice/Java/LongRunningService.java
+++ ServiceBestPractice/Java/LongRunningService.java
@@ -1,18 +1,42 @@
 package com.example.servicebestpractice;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+
+import java.util.Date;
 
 /**
  * Created by JingQBoom on 2016/4/24.
  */
-public class AlarmReceiver extends BroadcastReceiver {
+public class LongRunningService extends Service {
 
     @Override
-    public void onReceive(Context context, Intent intent){
-        Intent i = new Intent(context, LongRunningService.class);
-        //构造出一个Intent对象,去启动LongRunningService这个服务
-        context.startActivity(i);
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("LongRunningService", "excuted at " + new Date().toString());
+            }
+        }).start();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        //创建一个AlarmManager对象
+        int anHour = 60 * 60 * 1000;//定义一个小时的时间
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AlarmReceiver.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+        //指定处理定时任务的广播接收器为AlarmReceiver
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+        //调用set()方法
+        return super.onStartCommand(intent, flags, startId);
     }
 }
